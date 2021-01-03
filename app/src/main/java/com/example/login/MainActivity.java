61eeeff078457ba97ad40d65fda019b4d7b866a3
Daniel <eduardomontero123@hotmail.com>
2021-01-02 21:20:43 -0500

    Login Commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void entrar(View view){
+        EditText nombreEditText = findViewById(R.id.nombreEditText);
+        Log.i("Nombre: ", nombreEditText.getText().toString());
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
