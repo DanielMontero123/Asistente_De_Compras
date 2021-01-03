e4ed7b1c5ef6c1eaaecc6927778aa135ef8aefb7
Alex Jimenez <jjimenez2439@uta.edu.ec>
2021-01-03 18:24:01 -0500

    Interfaz creación de usuario

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -11,9 +11,11 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     public void entrar(View view){
-        EditText nombreEditText = findViewById(R.id.nombreEditText);
-        Log.i("Nombre: ", nombreEditText.getText().toString());
-
+        /*EditText cedulaEditText = findViewById(R.id.nombreEditText);
+        Log.i("Cédula: ", cedulaEditText.getText().toString());
+        */
+        Intent usuarios = new Intent(this, Usuarios.class);
+        startActivity(usuarios);
     }
 
     @Override
