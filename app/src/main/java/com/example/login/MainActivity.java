f5a1034fdd56b4666c3b277aad6c102a7c4cdd37
PAULO <paulomartinez1999@gmail.com>
2021-01-03 20:43:32 -0500

    Agrgado el boton registrar y conectado los boyons con su respectivo activity

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -7,19 +7,37 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void entrar(View view){
-        EditText nombreEditText = findViewById(R.id.nombreEditText);
-        Log.i("Nombre: ", nombreEditText.getText().toString());
 
-    }
-
+    private EditText nombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nombre = (EditText)findViewById(R.id.nombreEditText);
+    }
+
+
+    public void principal(View view){
+        //EditText nombreEditText = findViewById(R.id.nombreEditText);
+        //Log.i("Nombre: ", nombreEditText.getText().toString());
+        String nom = nombre.getText().toString();
+
+        if( !nom.isEmpty() ){
+            Intent principal = new Intent(this, Principal.class);
+            startActivity(principal);
+        }else{
+            Toast.makeText(this, "El usuario no existe", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void registrar(View view){
+        Intent usuarios = new Intent(this, Usuarios.class);
+        startActivity(usuarios);
     }
 
     //Metodo de prueba
--- app/src/main/java/com/example/login/Usuarios.java
+++ app/src/main/java/com/example/login/Usuarios.java
@@ -6,27 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class Usuarios extends AppCompatActivity {
     ArrayList<String> usuario = new ArrayList<>();
-    private EditText cedula;
+    private EditText cedula, nombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuarios);
         cedula = findViewById(R.id.TxtCed);
+        nombre = findViewById(R.id.TxtNom);
     }
 
     public void guardarUsuarios(View view){
-        usuario.add(cedula.getText().toString());
-        Intent categorias = new Intent(this, CategoriasProductos.class);
-        startActivity(categorias);
+        String ced = cedula.getText().toString();
+        String nom = nombre.getText().toString();
+        if( !ced.isEmpty() || !nom.isEmpty()  ){
+            usuario.add(cedula.getText().toString());
+            Intent categorias = new Intent(this, CategoriasProductos.class);
+            startActivity(categorias);
+        }else{
+            Toast.makeText(this, "Llene los campos", Toast.LENGTH_SHORT).show();
+        }
+
     }
-
-
-
 
 }
